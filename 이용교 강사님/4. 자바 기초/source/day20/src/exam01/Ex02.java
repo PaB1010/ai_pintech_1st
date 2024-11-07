a57bb5de41830ca02201fa2565ee93c1f44a3f9f
PaB1010 <mudodd@naver.com>
2024-11-07 17:39:04 +0900

    Add files via upload

---
+++ 이용교 강사님/4. 자바 기초/source/day20/src/exam01/Ex02.java
@@ -0,0 +1,22 @@
+// reduce 활용
+
+package exam01;
+
+import java.util.Arrays;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        int[] nums = {20, 10, 5, 99, 30, 21, 45};
+
+        int max = Arrays.stream(nums).reduce((a, b) -> a > b ? a : b).orElse(-1);
+
+        System.out.println("최대값 : " + max);
+        // 최대값 : 99
+
+        int min = Arrays.stream(nums). reduce(Math::min).orElse(-1);
+
+        System.out.println("최소값 : " + min);
+        // 최소값 : 5
+    }
+}
