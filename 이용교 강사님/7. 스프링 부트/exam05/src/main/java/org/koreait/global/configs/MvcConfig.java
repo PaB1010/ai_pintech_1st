c56c492b6dc4046052c0bb7d2d71bc8dc0f7597a
PaB1010 <mudodd@naver.com>
2024-11-12 18:21:44 +0900

    Add files via upload

---
+++ 이용교 강사님/7. 스프링 부트/exam05/src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -0,0 +1,12 @@
+package org.koreait.global.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// @EnableWebMvc = 불필요
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    
+    
+}
