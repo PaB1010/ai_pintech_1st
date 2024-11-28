1b5ee6807e8b503bc4234317e99448a925c2a26b
PaB1010 <mudodd@naver.com>
2024-11-29 01:15:34 +0900

    de

--- 이용교 강사님/7. 스프링 부트/exam05/src/main/java/org/koreait/board/repositories/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package org.koreait.board.repositories;
-
-import org.koreait.board.entities2.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface BoardRepository extends JpaRepository<Board, String>, QuerydslPredicateExecutor<Board> {
-}
