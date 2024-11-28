903e74177d5017b0a8921233e31b216b853b38ff
PaB1010 <mudodd@naver.com>
2024-11-29 01:09:24 +0900

    del

--- 이용교 강사님/exam05/src/main/java/org/koreait/member/repositories/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package org.koreait.member.repositories;
-
-import org.koreait.member.entities.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
