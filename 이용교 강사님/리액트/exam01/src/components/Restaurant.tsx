76483da3ba9eaf2450eb5eb3c8e73c63e3e54195
PaB1010 <mudodd@naver.com>
2025-01-31 11:03:05 +0900

    Add files via upload

--- 이용교 강사님/리액트/exam01/src/components/Restaurant.tsx
+++ 이용교 강사님/리액트/exam01/src/components/Restaurant.tsx
@@ -1,24 +1,45 @@
 'use client'
 
 // useLayoutEffect = Component 출력 전에 해야 할 작업 정의하는 곳
-import { use, useLayoutEffect, useState } from 'react'
+import { useLayoutEffect, useState } from 'react'
+
+type RestaurantType = {
+  seq: number
+  category: string
+  address: string
+  name: string
+  latitude: number
+  longitude: number
+}
 
 const Restaurant = () => {
   const [limit, setLimit] = useState<number>(10)
-
-  const [items, setItems] = useState({})
+  const [items, setItems] = useState<RestaurantType | undefined>()
 
   useLayoutEffect(() => {
     const url = `http://restaurant-service.koreait.xyz/restaurant/search?lat=37.5106297&lon=126.7246897&limit=${limit}`
     ;(async () => {
       const res = await fetch(url)
-
       const items = await res.json()
-
-      console.log(items)
+      setItems(items)
     })()
-  }, [])
-  return <></>
+  }, [items, limit])
+  console.log('items', items)
+  return (
+    <ul>
+      {items &&
+        items.map((item) => <RestaurantItem key={item.seq} item={item} />)}
+    </ul>
+  )
 }
+
+const RestaurantItem = ({ item }): React.ReactNode => {
+  const { category, name } = item
+  return (
+    <li>
+      <span>{category}</span>
+      <span>{name}</span>
+    </li>
+  )
 
 export default Restaurant
